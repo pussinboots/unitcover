7affbe4c783b91c2ca546ed3c39d68e455bd9847
Frank <pussinboots666@googlemail.com>
2014-06-18 16:37:14 +0200

    - fixed DBSpec test
    The jdbc url contains parameter useSSL=true that is not needed for that project.

--- test/unit/org/stock/manager/DBSpec.scala
+++ test/unit/org/stock/manager/DBSpec.scala
@@ -10,13 +10,18 @@ class DBSpec extends PlaySpecification {
   "DB" should {
     "given no jdbc url as CLEARDB_DATABASE_URL property return local jdbc url" in {
       val dburl = DB.parseDbUrl()
-      dburl._1 must beEqualTo("jdbc:mysql://127.0.0.1:3306/unitcover?autoReconnectForPools=true&reconnect=true&useSSL=true&useUnicode=yes&characterEncoding=UTF-8")
+      dburl._1 must beEqualTo("jdbc:mysql://127.0.0.1:3306/unitcover?autoReconnectForPools=true&reconnect=true&useUnicode=yes&characterEncoding=UTF-8")
       dburl._2 must beEqualTo("root")
       dburl._3 must beEqualTo("mysql")
     }
+    "given any jdbc url as CLEARDB_DATABASE_URL property don't use ssl database communication" in {
+      val dburl = DB.parseDbUrl()
+      dburl._1 must not contain("useSSL=true")    
+    }
+
     "given default CLEARDB_DATABASE_URL property return mysql datasource" in {
       val datasource = DB.getSlickMysqlDataSource()
-      datasource.getJdbcUrl() must beEqualTo("jdbc:mysql://127.0.0.1:3306/unitcover?autoReconnectForPools=true&reconnect=true&useSSL=true&useUnicode=yes&characterEncoding=UTF-8")
+      datasource.getJdbcUrl() must beEqualTo("jdbc:mysql://127.0.0.1:3306/unitcover?autoReconnectForPools=true&reconnect=true&useUnicode=yes&characterEncoding=UTF-8")
       datasource.getDriverClass() must beEqualTo("com.mysql.jdbc.Driver")
       datasource.getUser() must beEqualTo("root")
       datasource.getPassword() must beEqualTo("mysql")
