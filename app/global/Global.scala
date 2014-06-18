9e8564b4d8538565549b519c953969c0f9b59f2f
Frank <pussinboots666@googlemail.com>
2014-06-18 18:39:35 +0200

    - disbale https redirect
    The https redirect make it impossible to call the api

--- app/global/Global.scala
+++ app/global/Global.scala
@@ -5,7 +5,7 @@ import play.api.mvc._
 import scala.concurrent.Future
 import model.DB
 
-object Global extends WithFilters(HTTPSRedirectFilter) with GlobalSettings
+object Global extends GlobalSettings
 {
     override def onStart(app: Application) {
       val enableDBSSL = app.configuration.getBoolean("enableDBSSL").getOrElse(true)
@@ -31,24 +31,4 @@ object Global extends WithFilters(HTTPSRedirectFilter) with GlobalSettings
     }
     override def onStop(app: Application) {
     }
-}
-
-object HTTPSRedirectFilter extends Filter {
-
-  def apply(nextFilter: (RequestHeader) => Future[SimpleResult])(requestHeader: RequestHeader): Future[SimpleResult] = {
-    //play uses lower case headers.
-    implicit val context = scala.concurrent.ExecutionContext.Implicits.global
-    requestHeader.headers.get("x-forwarded-proto") match {
-      case Some(header) => {
-        if ("https" == header) {
-          nextFilter(requestHeader).map { result =>
-            result.withHeaders(("Strict-Transport-Security", "max-age=31536000"))
-          }
-        } else {
-          Future.successful(Results.Redirect("https://" + requestHeader.host + requestHeader.uri, 301))
-        }
-      }
-      case None => nextFilter(requestHeader)
-    }
-  }
 }
