4b110edc589fe3ac7d94b3572cbbc975847722de
Frank <pussinboots666@googlemail.com>
2014-06-20 23:54:04 +0200

    - detect travis build id and send it to unitcover

--- scripts/upload_junitreport.sh
+++ scripts/upload_junitreport.sh
@@ -4,8 +4,17 @@ project=unitcover
 #endpoint=localhost:9000
 endpoint=unitcover.herokuapp.com
 FILES=./target/test-reports/*
+trigger="trigger=Travis"
+travisBuildId=""
+ if [[ -n ${TRAVIS} ]]
+    then
+        echo "travis build detected"
+        travisBuildId="&travisBuildId=$TRAVIS_BUILD_ID"
+    else
+        trigger="trigger=Local"
+    fi
 #upload play junit reports
-buildnumber=$(curl -s -X POST http://$endpoint/api/$owner/$project/builds | sed -E 's/.*"buildNumber":([0-9]*).*/\1/')
+buildnumber=$(curl -s -X POST http://$endpoint/api/$owner/$project/builds?$trigger$travisBuildId | sed -E 's/.*"buildNumber":([0-9]*).*/\1/')
 echo $buildnumber
 echo "http://$endpoint/api/$owner/$project/$buildnumber"
 for f in $FILES
