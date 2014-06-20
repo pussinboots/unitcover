f076aa01ae960f12635e3a832cfd1c770f9a6b71
Frank <pussinboots666@googlemail.com>
2014-06-21 00:24:41 +0200

    - this test check now that no truststore and keystore is configured

--- test/integration/ApplicationSpec.scala
+++ test/integration/ApplicationSpec.scala
@@ -7,21 +7,24 @@ import play.api.test.Helpers._
 class ApplicationSpec extends PlaySpecification {
   sequential
   "application setup should" should {
-    "configured with custom keystore is enabled" in {
+    "no truststore and keystore are configured" in {
       running(FakeApplication()) {
         val keyStoreFile = System.getProperty("javax.net.ssl.keyStore")
         val keyStorePassword = System.getProperty("javax.net.ssl.keyStorePassword")
-        keyStoreFile must beEqualTo("ssl/keystore")
-        keyStorePassword must beEqualTo("")
-      }
-    }
-
-    "configured with custom truststore is enabled" in {
-      running(FakeApplication()) {
         val trustStoreFile = System.getProperty("javax.net.ssl.trustStore")
         val trustStorePassword = System.getProperty("javax.net.ssl.trustStorePassword")
-        trustStoreFile must beEqualTo("ssl/cacerts")
-        trustStorePassword must beEqualTo("")
+        trustStoreFile must beEqualTo(null)
+        trustStorePassword must beEqualTo(null)
+        keyStoreFile must beEqualTo(null)
+        keyStorePassword must beEqualTo(null)
+      }
+    }
+    "enable DB logging" in {
+      running(FakeApplication(additionalConfiguration=Map("enablePoolLogging" -> "true"))) {
+        val loggingClass = System.getProperty("com.mchange.v2.log.MLog")
+        val loggingLevel = System.getProperty("com.mchange.v2.log.FallbackMLog.DEFAULT_CUTOFF_LEVEL")
+        loggingClass must beEqualTo("com.mchange.v2.log.FallbackMLog")
+        loggingLevel must beEqualTo("ALL")
       }
     }
 
@@ -45,18 +48,6 @@ class ApplicationSpec extends PlaySpecification {
   }
 
   "application changed setup will work" should {
-    "disable db ssl" in {
-      running(FakeApplication(additionalConfiguration=Map("enableDBSSL" -> "false"))) {
-        val keyStoreFile = System.getProperty("javax.net.ssl.keyStore")
-        val keyStorePassword = System.getProperty("javax.net.ssl.keyStorePassword")
-        val trustStoreFile = System.getProperty("javax.net.ssl.trustStore")
-        val trustStorePassword = System.getProperty("javax.net.ssl.trustStorePassword")
-        trustStoreFile must beEqualTo(null)
-        trustStorePassword must beEqualTo(null)
-        keyStoreFile must beEqualTo(null)
-        keyStorePassword must beEqualTo(null)
-      }
-    }
     "enable DB logging" in {
       running(FakeApplication(additionalConfiguration=Map("enablePoolLogging" -> "true"))) {
         val loggingClass = System.getProperty("com.mchange.v2.log.MLog")
