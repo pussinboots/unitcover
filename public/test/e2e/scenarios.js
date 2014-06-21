3cf330332472b2937313e3d2267b1629625e756e
Frank <pussinboots666@googlemail.com>
2014-06-21 04:35:53 +0200

    - fixed e2e test and add a new one for the test suite site

--- public/test/e2e/scenarios.js
+++ public/test/e2e/scenarios.js
@@ -2,7 +2,7 @@
 
 /* http://docs.angularjs.org/guide/dev_guide.e2e-testing */
 
-describe('UnitCover', function() {
+describe('builds', function() {
 
   it('should redirect products.html to products.html#/builds', function() {
     browser().navigateTo('products.html');
@@ -19,31 +19,14 @@ describe('UnitCover', function() {
       expect(repeater('li.build').count()).toBe(10);
     });
     
-    it('the latest build show as first', function() {
-      expect(repeater('li.build').count().row(0).column('build.name')).toEqual("testsuite 11");
+    it('the latest build with buildNumber 11 show as first', function() {
+      expect(repeater('li.build:eq(0)').column('build.buildNumber')).toEqual(["11"]);
     });
 
-    /*it('the latest ten test suites should be display started with the eleven test suite', function() {
-      expect(repeater('li.build').column('build.name')).
-          toEqual(["testsuite 11", "testsuite 10", "testsuite 9","testsuite 8", "testsuite 7","testsuite 6", "testsuite 5","testsuite 4", "testsuite 3", "testsuite 2"]);
-    });*/
-
-    /*describe('display test cases of complete test suite', function() {
-
-      beforeEach(function() {
-        browser().navigateTo('products.html#/builds/pussinboots/bankapp/1');
-      });
-
-      it('ten test cases are display', function() {
-        expect(repeater('li.tescase').count()).toBe(10);
-      });
-
-      //problem that field name_enc is empty for that expect here but than setted right
-      it('display all test cases', function() {
-        expect(repeater('li.testcase').column('testcase.name')).
-            toEqual(["testsuite 11", "testsuite 10", "testsuite 9","testsuite 8", "testsuite 7","testsuite 6", "testsuite 5","testsuite 4", "testsuite 3", "testsuite 2"]);
-      });
-    });*/
+    it('the latest ten builds should be display', function() {
+      expect(repeater('li.build').column('build.buildNumber')).
+          toEqual(["11", "10", "9","8", "7","6", "5","4", "3", "2"]);
+    });    
   });
 
 
---
+++ public/test/e2e/testsuites.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/* http://docs.angularjs.org/guide/dev_guide.e2e-testing */
+
+describe('testsuites', function() {
+
+  it('should redirect products.html to products.html#/builds', function() {
+    browser().navigateTo('products.html');
+    expect(browser().location().url()).toBe('/builds');
+  });
+
+  describe('build with eleven test suites', function() {
+
+    beforeEach(function() {
+      browser().navigateTo('products.html#/builds/pussinboots/bankapp/testsuites/11');
+    });
+
+    it('ten test suites are display', function() {
+      expect(repeater('li.build').count()).toBe(1);
+    });
+    
+    it('the latest testsuite show as first', function() {
+      expect(repeater('li.build:eq(0)').column('build.name')).toEqual(["testsuite 11"]);
+      expect(element('li.build:eq(0) > span.status').attr('class')).toEqual("status ng-binding green");
+    });
+
+    it('the latest ten test suites should be display started with the eleven test suite', function() {
+      expect(repeater('li.build').column('build.name')).
+          toEqual(["testsuite 11"]);
+    });
+  });
+});
