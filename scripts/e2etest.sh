ec26d6ab53fa2b228eca18b41da80bc20784abeb
Frank <pussinboots666@googlemail.com>
2014-06-21 16:14:25 +0200

    - end 2 end test was broken after play 2.3 migration
    After the migration from 2.2 to 2.3 play version the start command work not
    as id did in the 2.2 version. I found an issue meantioned that behavoir change
    https://github.com/playframework/playframework/issues/2852 . But after a little
    reading of the play 2.3 documentation i found a wonderful solution for that i also
    describe it in the issue above.

--- scripts/e2etest.sh
+++ scripts/e2etest.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 scripts/bliss scripts/stop_play.sh
-sbt -Dconfig.file=conf/application-e2e.conf start & 
+sbt stage
+mkdir -p logs
+target/universal/stage/bin/unitcover -Dconfig.resource=application-e2e.conf &>logs/application.log 
 wget -O/dev/null --retry-connrefused --tries=40 http://localhost:9000/products.html 
 ./node_modules/.bin/karma start karma.conf-e2e.js --single-run && scripts/stop_play.sh
