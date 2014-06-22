e7e129e293f33f9f38a9f6a06c2a573cec7a434d
Frank <pussinboots666@googlemail.com>
2014-06-22 11:43:18 +0200

    - reduce the timeout for waiting play server is ready [skip ci]

--- scripts/e2etest.sh
+++ scripts/e2etest.sh
@@ -3,5 +3,5 @@ scripts/bliss scripts/stop_play.sh
 sbt stage
 mkdir -p logs
 target/universal/stage/bin/unitcover -Dconfig.resource=application-e2e.conf &>logs/application.log 
-wget -O/dev/null --retry-connrefused --tries=15 http://localhost:9000/products.html 
+wget -O/dev/null --retry-connrefused --tries=8 http://localhost:9000/products.html 
 ./node_modules/.bin/karma start karma.conf-e2e.js --single-run && scripts/stop_play.sh
