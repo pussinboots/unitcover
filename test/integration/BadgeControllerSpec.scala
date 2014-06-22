3661127999d3e41d23b5be44465466e1882366a5
Frank <pussinboots666@googlemail.com>
2014-06-22 15:14:16 +0200

    - added new test case for BadgeController class

---
+++ test/integration/BadgeControllerSpec.scala
@@ -0,0 +1,70 @@
+package integration
+
+import org.specs2.mutable.Specification
+import play.api.test._
+import play.api.test.Helpers._
+import play.api.libs.json.Json
+import play.api.libs.ws._
+import play.api.Play
+import play.api.Play.current
+import org.specs2.execute.AsResult
+import unit.org.stock.manager.test.DatabaseSetupBefore
+import test.E2ETestGlobal
+
+import model.{DB, Build}
+import scala.slick.jdbc.JdbcBackend.Database.dynamicSession
+
+import controllers.BadgeController
+
+class BadgeControllerSpec extends PlaySpecification with DatabaseSetupBefore {
+	sequential
+	import DB.dal._
+	import DB.dal.profile.simple._
+	def around[T: AsResult](f: => T) = {
+    running(FakeApplication()) {
+      E2ETestGlobal.onStart(Play.application)
+      DB.db withDynSession {
+        AsResult(f)
+      }
+    }
+  }
+
+	"BadgeController" should {
+		"tests passed build return tests passed badge" in {around{
+		    val build = Builds.findByBuildNumber(7).firstOption
+		    val badgeUrl = BadgeController.badgeUrl(build)
+		    val desc = "passed"
+		    val count = 7
+		    val color = "brightgreen"
+		    badgeUrl must equalTo(s"http://img.shields.io/badge/test-$desc20$count-$color.svg")
+		  }	
+		}
+		
+		"tests failed build return tests failed badge" in {around{
+		    val build = Builds.findByBuildNumber(9).firstOption
+		    val badgeUrl = BadgeController.badgeUrl(build)
+		    val desc = "failed"
+		    val count = 1
+		    val color = "yellow"
+		    badgeUrl must equalTo(s"http://img.shields.io/badge/test-$desc20$count-$color.svg")
+		  }	
+		}
+		
+		"tests errored build return tests error badge" in {around{ 
+		    val build = Builds.findByBuildNumber(10).firstOption
+		    val badgeUrl = BadgeController.badgeUrl(build)
+		    val desc = "error"
+		    val count = 1
+		    val color = "red"
+		    badgeUrl must equalTo(s"http://img.shields.io/badge/test-$desc20$count-$color.svg")
+		  }	
+		}
+		
+		"build not exists return tests unknown" in {around{ 
+		    val build = Builds.findByBuildNumber(200).firstOption
+		    val badgeUrl = BadgeController.badgeUrl(build)
+		    badgeUrl must equalTo("http://img.shields.io/badge/test-unknown-lightgrey.svg")
+		  }	
+		}
+	}
+}
