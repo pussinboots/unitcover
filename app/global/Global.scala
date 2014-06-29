b53b438539609a482e1d8aa72df6c45aac3b9c0f
Frank <pussinboots666@googlemail.com>
2014-06-29 16:57:15 +0200

    - added allow origin headers to all responses

--- app/global/Global.scala
+++ app/global/Global.scala
@@ -5,7 +5,7 @@ import play.api.mvc._
 import scala.concurrent.Future
 import model.DB
 
-object Global extends GlobalSettings {
+object Global extends GlobalSettings with WithFilters(Cors) {
   override def onStart(app: Application) {
     val enablePoolLogging = app.configuration.getBoolean("enablePoolLogging").getOrElse(false)
     if(enablePoolLogging) {
@@ -13,6 +13,34 @@ object Global extends GlobalSettings {
     } else {
       System.clearProperty("com.mchange.v2.log.MLog")
       System.clearProperty("com.mchange.v2.log.FallbackMLog.DEFAULT_CUTOFF_LEVEL")
+    }
+  }
+}
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import play.api.mvc._
+import play.api.mvc.Results._
+import play.api.http.HeaderNames._
+
+object Cors extends Filter {
+  def apply(next: (RequestHeader) => Future[SimpleResult])(request: RequestHeader): Future[SimpleResult] = {
+    val origin = request.headers.get(ORIGIN).getOrElse("*")
+    if (request.method == "OPTIONS") {
+      val response = Ok.withHeaders(
+        ACCESS_CONTROL_ALLOW_ORIGIN -> origin,
+        ACCESS_CONTROL_ALLOW_METHODS -> "POST, GET, OPTIONS, PUT, DELETE",
+        ACCESS_CONTROL_MAX_AGE -> "3600",
+        ACCESS_CONTROL_ALLOW_HEADERS -> s"$ORIGIN, X-Requested-With, $CONTENT_TYPE, $ACCEPT, $AUTHORIZATION, X-Auth-Token",
+        ACCESS_CONTROL_ALLOW_CREDENTIALS -> "true"
+      )
+      Future.successful(response)
+    } else {
+      next(request).map {
+        res => res.withHeaders(
+          ACCESS_CONTROL_ALLOW_ORIGIN -> origin,
+          ACCESS_CONTROL_ALLOW_CREDENTIALS -> "true"
+        )
+      }
     }
   }
 }
