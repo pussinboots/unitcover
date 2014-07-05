ac1fffefd3cb808d15261163e7d0f5c1810f8d19
Frank <pussinboots666@googlemail.com>
2014-07-05 12:10:12 +0200

    Removed not needed scripts anymore.
    The script upload_junitreport.sh is now downloaded from the unitcover server.
    Removed locale2etest.sh script the faste2e.sh script can be used instead by
    called with dev argument. Thi script performs the following steps
    
    - compile coffee scripts
    - instrument with istanbul for coverage
    - check for running application on localhost:9000 with wget
    - start karma run with the specified config (dev is default)

--- scripts/faste2e.sh
+++ scripts/faste2e.sh
@@ -1,6 +1,10 @@
-#!/bin/sh
+#!/bin/bash
+
+karmaCfg=${1:-dev}
+
 rm -rv public/js/bankapp/coverage
 ./node_modules/coffee-script/bin/coffee -c -o public/js/bankapp/coffee public/js/bankapp/coffee
 ./node_modules/istanbul/lib/cli.js instrument public/js/bankapp/ -o public/js/bankapp/coverage
 wget -O/dev/null --retry-connrefused --tries=2 http://localhost:9000/ 
-scripts/karma.sh dev
+echo "start karma with $karmaCfg"
+scripts/karma.sh ${karmaCfg}
