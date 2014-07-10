b65c638128441803dcecd53e34aea8ddaad74dad
Frank <pussinboots666@googlemail.com>
2014-07-10 09:02:01 +0200

    disable smoke production tests

--- scripts/e2etest.sh
+++ scripts/e2etest.sh
@@ -9,4 +9,4 @@ target/universal/stage/bin/unitcover -Dconfig.resource=application-e2e.conf &>lo
 wget -O/dev/null --retry-connrefused --tries=8 http://localhost:9000/ 
 scripts/karma.sh e2e 
 scripts/stop_play.sh
-scripts/karma.sh e2elive
+#scripts/karma.sh e2elive
