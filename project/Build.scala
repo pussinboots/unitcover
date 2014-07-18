3951d7aeb25a1a8d07dae6b046e180111daf1d07
Frank <pussinboots666@googlemail.com>
2014-07-19 00:03:19 +0200

    clean up runhook

--- project/Build.scala
+++ project/Build.scala
@@ -1,4 +1,4 @@
-object SimplePlayRunHook {
+object NPMPlayRunHook {
   import java.net.InetSocketAddress
   import play.PlayRunHook
   import sbt._
@@ -8,19 +8,16 @@ object SimplePlayRunHook {
     new PlayRunHook {
       override def beforeStarted(): Unit = {
         val cmdNpm = Seq("sh", "-c", "npm install")
-
         (cmdNpm  #||  "echo npm is missing and needed for local development to fetch the nodejs and bower dependencies. For install look here https://github.com/npm/npm. Perform npm task manual from your command line. Ignore it during herku deployment npm install will be performed fro the nodejs buildpack." !)
         /*val cmdCoffee = Seq("./node_modules/coffee-script/bin/coffee", "-c", "-o", "public/js/bankapp/coffee", "public/js/bankapp/coffee")
         (cmdCoffee  #||  "echo npm is missing and needed for local development to fetch the nodejs and bower dependencies. For install look here https://github.com/npm/npm. Perform npm task manual from your command line. Ignore it during herku deployment npm install will be performed fro the nodejs buildpack." !)*/
-       println(s"++++ compiled coffee script") 
+        println(s"++++ perform npm install when npm command can be found") 
       }
  
       override def afterStarted(address: InetSocketAddress): Unit = {
-        println(s"++++ simplePlayRunHook.afterStarted: $address")
       }
  
       override def afterStopped(): Unit = {
-        println("++++ simplePlayRunHook.afterStopped")
       }
     }
   }
